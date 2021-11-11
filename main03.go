7feee55fd7b26ce306e1419b6d257a267240d4c0
YusukeMuto <74335173+youse912215@users.noreply.github.com>
2021-11-11 14:13:20 +0900

    Add new function.

--- main03.go
+++ main03.go
@@ -2,15 +2,36 @@ package main
 
 import "fmt"
 
-int main() {
+func culc(x int, y int, v int) int {
+	return x*y + v
+}
+
+func main() {
 
 	var x int = 5
-	const max = 2
+	max := 2
 
 	fmt.Println("Before:", x)
 
 	if x > max {
-		max = x
+		x = max
 		fmt.Println("After:", x)
-	}	
+	}
+
+	y := 8
+
+	for x < y {
+		x++
+		fmt.Println("for:", x)
+		if x == 6 {
+			fmt.Println("break")
+			break
+		}
+	}
+
+	add1 := 2
+	add2 := 3
+
+	x = culc(add1, add2, x)
+	fmt.Println("Add:", x)
 }
