fb33739d36cca41422ab031d798b121308844ada
YusukeMuto <74335173+youse912215@users.noreply.github.com>
2021-11-11 13:30:22 +0900

    Add value.

--- practice01.go
+++ practice01.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	var (
+		aaa int     = 123
+		bbb float64 = 456
+	)
+
+	var name, age = "Yamada", 26
+
+	const foo = 1000
+
+	const (
+		foo2 = 10000
+		baa  = 39
+	)
+
 	fmt.Println("Hello, world")
+	fmt.Println("aaa =", aaa, "bbb =", bbb)
+	fmt.Println("name =", name, "age =", age)
 }
